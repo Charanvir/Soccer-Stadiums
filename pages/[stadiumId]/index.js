a2728eb02a39fa24a1143c2b22ccf2729836d18e
Charanvir <charanvir123@gmail.com>
2022-09-13 02:55:06 -0400

    added Stadium Details page

---
+++ pages/[stadiumId]/index.js
@@ -0,0 +1,15 @@
+// domain.com/[stadiumId]
+import MeetupDetail from "../../components/meetups/MeetupDetail"
+
+const StadiumDetails = () => {
+    return (
+        <MeetupDetail
+            image='https://dd20lazkioz9n.cloudfront.net/wp-content/uploads/2022/03/Copy-of-WP-News-story-template-2022-03-15T093911.099.jpg'
+            title='Old Trafford'
+            address='Sir Matt Busby Way, Old Trafford, Stretford, Manchester M16 0RA, UK'
+            description='The home stadium of one of Europes most prestige clubs, Manchester United.'
+        ></MeetupDetail>
+    )
+}
+
+export default StadiumDetails
