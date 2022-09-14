60476c53b492abecb744ff842b9ddca3d10516fd
Charanvir <charanvir123@gmail.com>
2022-09-13 21:07:50 -0400

    added post request to add new stadiums to database

--- pages/new-stadium/index.js
+++ pages/new-stadium/index.js
@@ -1,9 +1,23 @@
 // domain.com/new-stadium
+import { useRouter } from 'next/router'
 import NewMeetupForm from '../../components/meetups/NewMeetupForm'
 
 const NewStadiumPage = () => {
-    const addStadiumHandler = (enteredStadiumData) => {
-        console.log(enteredStadiumData)
+    const router = useRouter()
+    const addStadiumHandler = async (enteredStadiumData) => {
+        const response = await fetch('/api/new-stadium', {
+            method: 'POST',
+            body: JSON.stringify(enteredStadiumData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+        const data = await response.json();
+
+        console.log(data)
+
+        router.push('/')
     }
 
     return (
