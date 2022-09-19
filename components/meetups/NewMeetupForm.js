39d5ef9fd4e144e415c301c085b4c1f40952dc84
Charanvir <charanvir123@gmail.com>
2022-09-18 22:01:43 -0400

    added additional form fields to reflect changes made to database to include additional information

--- components/meetups/NewMeetupForm.js
+++ components/meetups/NewMeetupForm.js
@@ -8,6 +8,12 @@ function NewMeetupForm(props) {
   const imageInputRef = useRef();
   const addressInputRef = useRef();
   const descriptionInputRef = useRef();
+  const cityInputRef = useRef();
+  const countryInputRef = useRef();
+  const capacityInputRef = useRef();
+  const dateOpenedInputRef = useRef();
+  const teamInputRef = useRef();
+  const categoryInputRef = useRef();
 
   function submitHandler(event) {
     event.preventDefault();
@@ -42,12 +48,39 @@ function NewMeetupForm(props) {
           <label htmlFor='address'>Address</label>
           <input type='text' required id='address' ref={addressInputRef} />
         </div>
+        <div className={classes.controlHalf}>
+          <label htmlFor='city'>City</label>
+          <input type='text' required id='city' ref={cityInputRef} />
+
+          <label htmlFor='country'>Country</label>
+          <input type='text' required id='countr' ref={countryInputRef} />
+        </div>
+        <div className={classes.controlHalf}>
+          <label htmlFor='capacity'>Capacity</label>
+          <input type='number' required id='capacity' ref={capacityInputRef} />
+
+          <label htmlFor='dateOpened'>Date Opened</label>
+          <input type='date' required id='dateOpened' ref={dateOpenedInputRef} />
+        </div>
+        <div className={classes.controlHalf}>
+          <label htmlFor='team'>Teams</label>
+          <input type='text' required id='team' ref={teamInputRef} />
+
+          <label htmlFor='category'>Category</label>
+          <select type='select' required id='category' ref={categoryInputRef}>
+            <option value='soccer'>Soccer</option>
+            <option value='football'>Football</option>
+            <option value='hockey'>Hockey</option>
+            <option value='basketball'>Basketball</option>
+            <option value='baseball'>Baseball</option>
+          </select>
+        </div>
         <div className={classes.control}>
           <label htmlFor='description'>Description</label>
           <textarea
             id='description'
             required
-            rows='5'
+            rows='2'
             ref={descriptionInputRef}
           ></textarea>
         </div>
